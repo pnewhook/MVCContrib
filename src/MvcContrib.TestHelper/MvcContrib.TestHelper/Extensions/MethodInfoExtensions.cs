8322369396220ce224e9c8297f1fbcc00fb9059d
mendicant <none@none>
2010-03-18 11:49:51 -0600

    Break apart files and add some documentation.

--- src/MvcContrib.TestHelper/MvcContrib.TestHelper/Extensions/MethodInfoExtensions.cs
+++ src/MvcContrib.TestHelper/MvcContrib.TestHelper/Extensions/MethodInfoExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Reflection;
 using System.Web.Mvc;
 
@@ -6,25 +5,17 @@ namespace MvcContrib.TestHelper
 {
     public static class MethodInfoExtensions
     {
+        /// <summary>
+        /// Will return the name of the action specified in the ActionNameAttribute for a method if it has an ActionNameAttribute.
+        /// Will return the name of the method otherwise.
+        /// </summary>
+        /// <param name="method"></param>
+        /// <returns></returns>
         public static string ActionName(this MethodInfo method)
         {
             if (method.IsDecoratedWith<ActionNameAttribute>()) return method.GetAttribute<ActionNameAttribute>().Name;
 
             return method.Name;
         }
-    }
-
-    public static class AttributeExtensions
-    {
-        public static bool IsDecoratedWith<T>(this ICustomAttributeProvider attributeTarget) where T : Attribute
-        {
-            return attributeTarget.GetCustomAttributes(typeof(T), false).Length > 0;
-        }
-
-        public static T GetAttribute<T>(this ICustomAttributeProvider attributeTarget) where T : Attribute
-        {
-            return (T)attributeTarget.GetCustomAttributes(typeof(T), false)[0];
-        }
-        
     }
 }
