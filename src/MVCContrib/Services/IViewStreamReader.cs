4af01ee9d6cd4a3588b76028b93ff13a5eea0dd6
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2010-03-21 11:04:53 +0000

    Replace spaces with tabs

--- src/MVCContrib/Services/IViewStreamReader.cs
+++ src/MVCContrib/Services/IViewStreamReader.cs
@@ -3,11 +3,11 @@ using System.Web.Mvc;
 
 namespace MvcContrib.Services
 {
-    /// <summary>
-    /// Reads view as a stream.
-    /// </summary>
-    public interface IViewStreamReader
-    {
-        Stream GetViewStream(string viewName, object model, ControllerContext context);
-    }
+	/// <summary>
+	/// Reads view as a stream.
+	/// </summary>
+	public interface IViewStreamReader
+	{
+		Stream GetViewStream(string viewName, object model, ControllerContext context);
+	}
 }
