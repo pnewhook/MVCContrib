8c048f42d092f497c3f9379b55027e0dcf80ef1b
erichexter <none@none>
2010-04-22 10:33:51 -0500

    Cleaned up the scenerios of using and writting portable areas.

--- src/MVCContrib/PortableAreas/PortableAreaRegistration.cs
+++ src/MVCContrib/PortableAreas/PortableAreaRegistration.cs
@@ -1,16 +1,45 @@
 using System;
 using System.Web.Mvc;
+using MvcContrib.UI.InputBuilder;
 using MvcContrib.UI.InputBuilder.ViewEngine;
 
 namespace MvcContrib.PortableAreas
 {
 	public abstract class PortableAreaRegistration:AreaRegistration
 	{
-		public abstract void RegisterArea(AreaRegistrationContext context,IApplicationBus bus);
-		
+		public virtual void RegisterArea(AreaRegistrationContext context,IApplicationBus bus)
+		{
+
+			bus.Send(new PortableAreaStartupMessage(AreaName));
+
+			RegisterDefaultRoutes(context);			
+		}
+
+		public  void CreateStaticResourceRoute(AreaRegistrationContext context, string SubfolderName)
+		{
+			context.MapRoute(
+			AreaName + "-" + SubfolderName,
+			AreaName + "/"+SubfolderName+"/{resourceName}",
+			new { controller = "EmbeddedResource", action = "Index", resourcePath = "Content."+SubfolderName },
+			null,
+			new string[] { "MvcContrib.PortableAreas" }
+			);
+		}
+
+		public void RegisterDefaultRoutes(AreaRegistrationContext context) {
+			CreateStaticResourceRoute(context, "Images");
+			CreateStaticResourceRoute(context, "Styles");
+			CreateStaticResourceRoute(context, "Scripts");
+			context.MapRoute(AreaName + "-Default",
+			                 AreaName + "/{controller}/{action}",
+			                 new { controller = "default", action = "index" });
+		}
+
 		public override void RegisterArea(AreaRegistrationContext context)
 		{
 			RegisterArea(context,Bus.Instance);
+			RegisterAreaEmbeddedResources();
+			InputBuilder.BootStrap(); //register the embedded view engine.
 		}
 
         public void RegisterAreaEmbeddedResources()
--- src/MVCContrib/UI/InputBuilder/InputBuilder.cs
+++ src/MVCContrib/UI/InputBuilder/InputBuilder.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Hosting;
 using System.Web.Mvc;
+using MvcContrib.PortableAreas;
 using MvcContrib.UI.InputBuilder.Conventions;
 using MvcContrib.UI.InputBuilder.InputSpecification;
 using MvcContrib.UI.InputBuilder.ViewEngine;
@@ -26,12 +28,17 @@ namespace MvcContrib.UI.InputBuilder
 
 		public static void BootStrap()
 		{
-			VirtualPathProvider pathProvider = new AssemblyResourceProvider();
+			if (!ViewEngines.Engines.Any(engine => engine.GetType().Equals(typeof(InputBuilderViewEngine))))
+			{
+				VirtualPathProvider pathProvider = new AssemblyResourceProvider();
 
-			RegisterPathProvider(pathProvider);
+				RegisterPathProvider(pathProvider);
 
-			//ViewEngines.Engines.Clear();
-			ViewEngines.Engines.Add(new InputBuilderViewEngine(new string[0]));
+				var resourceStore = new AssemblyResourceStore(typeof(PortableAreaRegistration), "/areas", typeof(PortableAreaRegistration).Namespace);
+				AssemblyResourceManager.RegisterAreaResources(resourceStore);
+
+				ViewEngines.Engines.Add(new InputBuilderViewEngine(new string[0]));
+			}
 		}
 
 		public static void SetPropertyConvention(Func<IList<IPropertyViewModelFactory>> conventionProvider)
