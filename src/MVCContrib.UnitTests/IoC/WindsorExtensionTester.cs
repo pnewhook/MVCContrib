b8d23d2d71252d1e5911f60387621af37c372640
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2010-07-27 18:54:45 +0100

    Fix some windsor tests
    --HG--
    branch : mvc3
    extra : rebase_source : a8cad8cff6e020b9a41290bbb22d5141a470cea1

--- src/MVCContrib.UnitTests/IoC/WindsorExtensionTester.cs
+++ src/MVCContrib.UnitTests/IoC/WindsorExtensionTester.cs
@@ -36,14 +36,14 @@ namespace MvcContrib.UnitTests.IoC
 			[Test]
 			public void Then_the_type_should_be_registered()
 			{
-				Assert.That(_container.Kernel.HasComponent("mvccontrib.unittests.castle.windsorcontrollerfactorytester+windsorsimplecontroller"));
-				Assert.That(_container.Kernel.GetHandler("mvccontrib.unittests.castle.windsorcontrollerfactorytester+windsorsimplecontroller").ComponentModel.Implementation, Is.EqualTo(typeof(WindsorSimpleController)));
+				Assert.That(_container.Kernel.HasComponent(typeof(WindsorSimpleController)));
+				Assert.That(_container.Kernel.GetHandler(typeof(WindsorSimpleController)).ComponentModel.Implementation, Is.EqualTo(typeof(WindsorSimpleController)));
 			}
 
 			[Test]
 			public void Then_the_lifestyle_should_be_transient()
 			{
-				Assert.That(_container.Kernel.GetHandler("mvccontrib.unittests.castle.windsorcontrollerfactorytester+windsorsimplecontroller").ComponentModel.LifestyleType, Is.EqualTo(LifestyleType.Transient));
+				Assert.That(_container.Kernel.GetHandler(typeof(WindsorSimpleController)).ComponentModel.LifestyleType, Is.EqualTo(LifestyleType.Transient));
 			}
 		}
 
@@ -59,14 +59,14 @@ namespace MvcContrib.UnitTests.IoC
 			[Test]
 			public void Then_the_type_should_be_registered()
 			{
-				Assert.That(_container.Kernel.HasComponent("mvccontrib.unittests.castle.windsorcontrollerfactorytester+windsorsimplecontroller"));
-				Assert.That(_container.Kernel.GetHandler("mvccontrib.unittests.castle.windsorcontrollerfactorytester+windsorsimplecontroller").ComponentModel.Implementation, Is.EqualTo(typeof(WindsorSimpleController)));
+				Assert.That(_container.Kernel.HasComponent(typeof(WindsorSimpleController)));
+				Assert.That(_container.Kernel.GetHandler(typeof(WindsorSimpleController)).ComponentModel.Implementation, Is.EqualTo(typeof(WindsorSimpleController)));
 			}
 
 			[Test]
 			public void Then_the_lifestyle_should_be_transient()
 			{
-				Assert.That(_container.Kernel.GetHandler("mvccontrib.unittests.castle.windsorcontrollerfactorytester+windsorsimplecontroller").ComponentModel.LifestyleType, Is.EqualTo(LifestyleType.Transient));
+				Assert.That(_container.Kernel.GetHandler(typeof(WindsorSimpleController)).ComponentModel.LifestyleType, Is.EqualTo(LifestyleType.Transient));
 			}
 		}
 
@@ -82,68 +82,23 @@ namespace MvcContrib.UnitTests.IoC
 			[Test]
 			public void Then_all_controllers_in_the_assembly_should_be_registered()
 			{
-				Assert.That(_container.Kernel.HasComponent("mvccontrib.unittests.castle.windsorcontrollerfactorytester+windsorsimplecontroller"));
-				Assert.That(_container.Kernel.GetHandler("mvccontrib.unittests.castle.windsorcontrollerfactorytester+windsorsimplecontroller").ComponentModel.Implementation, Is.EqualTo(typeof(WindsorSimpleController)));
+				Assert.That(_container.Kernel.HasComponent(typeof(WindsorSimpleController)));
+				Assert.That(_container.Kernel.GetHandler(typeof(WindsorSimpleController)).ComponentModel.Implementation, Is.EqualTo(typeof(WindsorSimpleController)));
 				//etc
 			}
 
 			[Test]
 			public void Then_lifestyles_should_be_set_to_transient()
 			{
-				Assert.That(_container.Kernel.GetHandler("mvccontrib.unittests.castle.windsorcontrollerfactorytester+windsorsimplecontroller").ComponentModel.LifestyleType, Is.EqualTo(LifestyleType.Transient));
-				Assert.That(_container.Kernel.GetHandler("mvccontrib.unittests.controllerfactories.ioccontrollerfactorytester.ioctestcontroller").ComponentModel.LifestyleType, Is.EqualTo(LifestyleType.Transient));
+				Assert.That(_container.Kernel.GetHandler(typeof(WindsorSimpleController)).ComponentModel.LifestyleType, Is.EqualTo(LifestyleType.Transient));
 			}
 		}
 
-		public class WindsorDisposableController : IDisposable, IController {
-			public bool IsDisposed;
-
-			public WindsorDisposableController() {
-				IsDisposed = false;
-			}
-
-			public void Dispose() {
-				IsDisposed = true;
-			}
-
-			public void Execute(RequestContext controllerContext) {
-			}
-		}
-
-		public class MockApplication : HttpApplication, IContainerAccessor {
-			private readonly IWindsorContainer _container;
-
-			public MockApplication(IWindsorContainer container) {
-				_container = container;
-			}
-
-			public IWindsorContainer Container {
-				get { return _container; }
-			}
-		}
 
 		public class WindsorSimpleController : IController {
 			public void Execute(RequestContext controllerContext) {
 				throw new NotImplementedException();
 			}
-		}
-
-		public class WindsorDependencyController : IController {
-			public IDependency _dependency;
-
-			public WindsorDependencyController(IDependency dependency) {
-				_dependency = dependency;
-			}
-
-			public void Execute(RequestContext controllerContext) {
-				throw new NotImplementedException();
-			}
-		}
-
-		public interface IDependency {
-		}
-
-		public class StubDependency : IDependency {
 		}
 	}
 }
