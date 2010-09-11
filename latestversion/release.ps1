4d5529d63c9cecf205cf618bb15ac4e3936652df
eric_hexter <none@none>
2010-09-11 12:05:37 -0500

    added assm name number

--- latestversion/release.ps1
+++ latestversion/release.ps1
@@ -1,17 +1,37 @@
+$dir = resolve-path .\
+
+function Extract-Package
+{
+	param([string]$zipfilename, [string] $destination)
+
+	if(test-path($zipfilename))
+	{	
+		$shellApplication = new-object -com shell.application
+		$zipPackage = $shellApplication.NameSpace($zipfilename)
+		$destinationFolder = $shellApplication.NameSpace($destination)
+		$destinationFolder.CopyHere($zipPackage.Items())
+	}
+}
+
 function DownloadFiles {
-    remove-item "MVCContrib.Extras.release.zip"
-    remove-item "MVCContrib.release.zip"
-    remove-item "MVCContrib.source.zip"
-    $extrasUrl  = "http://build1.headspringlabs.com/guestAuth/repository/download/bt2/.lastPinned/MVCContrib.Extras.release.zip"
-    $releaseUrl = "http://build1.headspringlabs.com/guestAuth/repository/download/bt2/.lastPinned/MVCContrib.release.zip"
-    $sourceUrl  = "http://build1.headspringlabs.com/guestAuth/repository/download/bt2/.lastPinned/MVCContrib.source.zip"
+    remove-item "MVCContrib.Extras.release.zip" -ErrorAction:SilentlyContinue
+    remove-item "MVCContrib.release.zip"  -ErrorAction:SilentlyContinue
+    remove-item "MVCContrib.source.zip"  -ErrorAction:SilentlyContinue
+    $extrasUrl  = "http://build-oss.headspringlabs.com/guestAuth/repository/download/bt2/.lastPinned/MVCContrib.Extras.release.zip"
+    $releaseUrl = "http://build-oss.headspringlabs.com/guestAuth/repository/download/bt2/.lastPinned/MVCContrib.release.zip"
+    $sourceUrl  = "http://build-oss.headspringlabs.com/guestAuth/repository/download/bt2/.lastPinned/MVCContrib.source.zip"
 
     $clnt = new-object System.Net.WebClient
 
-    $clnt.DownloadFile($extrasUrl,"MVCContrib.Extras.release.zip")
-    $clnt.DownloadFile($releaseUrl,"MVCContrib.release.zip")
-    $clnt.DownloadFile($sourceUrl,"MVCContrib.source.zip")
+    $clnt.DownloadFile($extrasUrl,"$($dir)MVCContrib.Extras.release.zip")
+    $clnt.DownloadFile($releaseUrl, "$($dir)\MVCContrib.release.zip")
+    $clnt.DownloadFile($sourceUrl, "$($dir)\MVCContrib.source.zip")
 }
 
-DownloadFiles 
-& "..\bin\codeplex\createrelease.exe" "2.0.35.0"
+#DownloadFiles 
+mkdir "release"
+Extract-PAckage "$($dir)\MVCContrib.release.zip" "$($dir)\release"
+
+[System.Diagnostics.FileVersionInfo]::GetVersionInfo("$($dir)\RELEASE\MvcContrib.dll").FileVersion
+
+#& "..\bin\codeplex\createrelease.exe" "2.0.87.0" 
