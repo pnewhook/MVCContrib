eea97c026b8218d7b6f5abde63e457388ba92091
eric_hexter <none@none>
2010-09-13 13:09:15 -0500

    do not download files.. just publish them.

--- latestversion/release.ps1
+++ latestversion/release.ps1
@@ -1,37 +1,26 @@
 $dir = resolve-path .\
 
-function Extract-Package
-{
-	param([string]$zipfilename, [string] $destination)
-
-	if(test-path($zipfilename))
-	{	
-		$shellApplication = new-object -com shell.application
-		$zipPackage = $shellApplication.NameSpace($zipfilename)
-		$destinationFolder = $shellApplication.NameSpace($destination)
-		$destinationFolder.CopyHere($zipPackage.Items())
-	}
-}
-
 function DownloadFiles {
     remove-item "MVCContrib.Extras.release.zip" -ErrorAction:SilentlyContinue
     remove-item "MVCContrib.release.zip"  -ErrorAction:SilentlyContinue
     remove-item "MVCContrib.source.zip"  -ErrorAction:SilentlyContinue
+    remove-item "MVCContrib.dll"  -ErrorAction:SilentlyContinue
+    
     $extrasUrl  = "http://build-oss.headspringlabs.com/guestAuth/repository/download/bt2/.lastPinned/MVCContrib.Extras.release.zip"
     $releaseUrl = "http://build-oss.headspringlabs.com/guestAuth/repository/download/bt2/.lastPinned/MVCContrib.release.zip"
     $sourceUrl  = "http://build-oss.headspringlabs.com/guestAuth/repository/download/bt2/.lastPinned/MVCContrib.source.zip"
-
+    $verDll =  "http://build-oss.headspringlabs.com/guestAuth/repository/download/bt2/.lastPinned/Mvc"
     $clnt = new-object System.Net.WebClient
 
-    $clnt.DownloadFile($extrasUrl,"$($dir)MVCContrib.Extras.release.zip")
+    $clnt.DownloadFile($extrasUrl,"$($dir)\MVCContrib.Extras.release.zip")
     $clnt.DownloadFile($releaseUrl, "$($dir)\MVCContrib.release.zip")
     $clnt.DownloadFile($sourceUrl, "$($dir)\MVCContrib.source.zip")
+    $clnt.DownloadFile($verDll, "$($dir)\MVCContrib.dll")
+    
 }
 
 #DownloadFiles 
-mkdir "release"
-Extract-PAckage "$($dir)\MVCContrib.release.zip" "$($dir)\release"
 
-[System.Diagnostics.FileVersionInfo]::GetVersionInfo("$($dir)\RELEASE\MvcContrib.dll").FileVersion
+$ver = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$($dir)\MvcContrib.dll").FileVersion
 
-#& "..\bin\codeplex\createrelease.exe" "2.0.87.0" 
+& "..\bin\codeplex\createrelease.exe" $ver 
