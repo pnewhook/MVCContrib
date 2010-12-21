ffdb61119ffb367db9df3cb2c104f4716d8c7ebc
Cameron Morrison <cameron.am@gmail.com>
2010-12-22 09:48:20 +1000

    Adds an assertion to check if a certian key has been passed to TempData.Keep(key).

---
+++ src/MVCContrib.UnitTests/TestHelper/TempDataTestExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using MvcContrib.TestHelper;
+using NUnit.Framework;
+
+namespace MvcContrib.UnitTests.TestHelper
+{
+    [TestFixture]
+    public class TempDataTestExtensionsTests
+    {
+        [Test]
+        public void PassesWhenKeyKept()
+        {
+            string key = "testkey";
+            TempDataDictionary tempData = new TempDataDictionary();
+            tempData.Add(key, new object());
+            tempData.Keep(key);
+            tempData.AssertKept(key);
+        }
+
+        [Test]
+        [ExpectedException(typeof(MvcContrib.TestHelper.AssertionException), ExpectedMessage="Key 'testkey' not kept.")]
+        public void FailsWhenKeyNotKept()
+        {
+            string key = "testkey";
+            TempDataDictionary tempData = new TempDataDictionary();
+            tempData.Add(key, new object());
+            tempData.Keep("nottestkey");
+            tempData.AssertKept(key);
+        }
+    }
+}
---
+++ src/MvcContrib.TestHelper/MvcContrib.TestHelper/Extensions/TempDataTestExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MvcContrib.TestHelper
+{
+    public static class TempDataTestExtensions
+    {
+        /// <summary>
+        /// Asserts that a key has been passed to TempData.Keep(key)
+        /// </summary>
+        /// <param name="TempData">TempData collection</param>
+        /// <param name="key">The key to assert kept.</param>
+        public static void AssertKept(this TempDataDictionary TempData, string key)
+        {
+            var keptKeysField = typeof(TempDataDictionary).GetField("_retainedKeys", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var keptKeys = keptKeysField.GetValue(TempData) as HashSet<string>;
+
+            if (!keptKeys.Contains(key))
+                throw new AssertionException(String.Format("Key '{0}' not kept.", key));
+        }
+    }
+}
