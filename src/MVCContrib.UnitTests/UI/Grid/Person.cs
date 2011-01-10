6800155c75b3212e7356f2f151d79acc863b6438
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2011-01-10 18:35:26 +0000

    Fix some failing tests

--- src/MVCContrib.UnitTests/UI/Grid/Person.cs
+++ src/MVCContrib.UnitTests/UI/Grid/Person.cs
@@ -1,12 +1,13 @@
 using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcContrib.UnitTests.UI.Grid
 {
 	public class Person
 	{
 		public string Name { get; set; }
-		[DisplayName("Name2")]
+		[DisplayName("Name2"), ScaffoldColumn(false)]
 		public string NameWithAttribute { get; set; }
 
 		public DateTime DateOfBirth { get; set; }
